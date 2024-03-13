f6f12ce33c468b90b678d0eb4ab8b1234cc65bb9
LettyCoach <laneandyumiko@gmail.com>
2024-03-13 15:01:02 +0100

    fix: contact form

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,24 +7,6 @@ import github from "./github.png";
 import menu from "./logo.png";
 import close from "./close.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import express from "./tech/express_white.svg";
-import reactjs from "./tech/reactjs.svg";
-import redux from "./tech/redux.png";
-import vuejs from "./tech/vue.svg";
-import nextjs from "./tech/nextjs_white.svg";
-import tailwind from "./tech/tailwind.svg";
-import typescript from "./tech/typescript.svg";
-import threejs from "./tech/threejs.svg";
-import sass_scss from "./tech/sass.svg";
-
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
@@ -43,23 +25,6 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  express,
-  reactjs,
-  redux,
-  vuejs,
-  nextjs,
-  tailwind,
-  typescript,
-  threejs,
-  sass_scss,
   meta,
   shopify,
   starbucks,
